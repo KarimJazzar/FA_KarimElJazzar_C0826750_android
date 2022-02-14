46dbc05cd298e31223ddec5323b0033f9ce0cc31
Karim El Jazzar <jazzar.karim@gmail.com>
2022-02-15 00:41:52 +0200

    Initial commit

---
+++ app/src/main/java/com/karim/fa_karimeljazzar_c0826750_android/MainActivity.java
@@ -0,0 +1,14 @@
+package com.karim.fa_karimeljazzar_c0826750_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
